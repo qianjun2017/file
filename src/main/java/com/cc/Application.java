cb9f9e87bc15ea346e6bc21515c2f2434b7aaa0b
yuanwenshu <yuanwenshu@cq.chinamobile.com>
2019-02-28 18:25:49 +0800

    修改

--- src/main/java/com/cc/Application.java
+++ src/main/java/com/cc/Application.java
@@ -5,20 +5,13 @@ package com.cc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * @author Administrator
  *
  */
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(Application.class);
-	}
+public class Application {
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
