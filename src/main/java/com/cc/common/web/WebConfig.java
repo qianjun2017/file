756c6b19230ee84775fa22877b68be29fadcedfc
yuanwenshu <yuanwenshu@cq.chinamobile.com>
2019-05-05 11:38:27 +0800

    修改

--- src/main/java/com/cc/common/web/WebConfig.java
+++ src/main/java/com/cc/common/web/WebConfig.java
@@ -3,26 +3,9 @@
  */
 package com.cc.common.web;
 
-import java.util.List;
-
 import org.springframework.context.annotation.Configuration;
-import org.springframework.format.FormatterRegistry;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.validation.MessageCodesResolver;
-import org.springframework.validation.Validator;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
-import org.springframework.web.servlet.HandlerExceptionResolver;
-import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.cc.common.spring.SpringContextUtil;
 import com.cc.file.config.FileConfig;
@@ -34,27 +17,7 @@ import com.cc.file.strategy.impl.NativeFileStrategy;
  *
  */
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-	@Override
-	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-
-	}
-
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-
-	}
-
-	@Override
-	public void addFormatters(FormatterRegistry registry) {
-
-	}
-
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-
-	}
+public class WebConfig extends WebMvcConfigurerAdapter {
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -63,71 +26,6 @@ public class WebConfig implements WebMvcConfigurer {
 			FileConfig fileConfig = (FileConfig)SpringContextUtil.getBean(FileConfig.class);
 			registry.addResourceHandler("/files/**").addResourceLocations("file:"+fileConfig.getLocation());
 		}
-	}
-
-	@Override
-	public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> returnValueHandlers) {
-
-	}
-
-	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-
-	}
-
-	@Override
-	public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
-
-	}
-
-	@Override
-	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-
-	}
-
-	@Override
-	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-
-	}
-
-	@Override
-	public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
-
-	}
-
-	@Override
-	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-
-	}
-
-	@Override
-	public void configurePathMatch(PathMatchConfigurer configurer) {
-
-	}
-
-	@Override
-	public void configureViewResolvers(ViewResolverRegistry registry) {
-
-	}
-
-	@Override
-	public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
-
-	}
-
-	@Override
-	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-
-	}
-
-	@Override
-	public MessageCodesResolver getMessageCodesResolver() {
-		return null;
-	}
-
-	@Override
-	public Validator getValidator() {
-		return null;
 	}
 
 }
