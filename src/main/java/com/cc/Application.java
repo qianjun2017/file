c85d6a0b83680a1a36f132e4182452e1558cc4e0
yuanwenshu <yuanwenshu@cq.chinamobile.com>
2019-12-11 17:27:23 +0800

    修改

--- src/main/java/com/cc/Application.java
+++ src/main/java/com/cc/Application.java
@@ -5,12 +5,17 @@ package com.cc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * @author Administrator
  *
  */
 @SpringBootApplication
+@EnableTransactionManagement
+@MapperScan(basePackages={"com.cc.system.*.mapper","com.cc.*.mapper","com.cc.system.*.dao","com.cc.*.dao"})
 public class Application {
 
 	public static void main(String[] args) throws Exception {
